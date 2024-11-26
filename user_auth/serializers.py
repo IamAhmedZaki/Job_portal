25f9aa019ec4f075ec2ff1aaf90c2b3e48c35c5f
IamAhmedZaki <mahmedzaki670@gmail.com>
2024-11-26 20:45:04 +0500

    Update serializers.py

--- user_auth/serializers.py
+++ user_auth/serializers.py
@@ -16,6 +16,7 @@ class LoginSerializer(serializers.Serializer):
 
 class SignupSerializer(serializers.Serializer):
     username = serializers.CharField()
+    email=serializers.CharField()
     password = serializers.CharField(write_only=True)
     role = serializers.ChoiceField(choices=[(tag, tag.value) for tag in StatusEnum], read_only=True)
     
